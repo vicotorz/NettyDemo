9840627c52385d56c6c706d657d0845aa155aef9
Victorz <victorzsnail@qq.com>
2019-04-20 23:00:20 +0800

    Netty Client

---
+++ src/main/java/EchoClientHandler.java
@@ -0,0 +1,27 @@
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.util.CharsetUtil;
+
+
+/**
+ * Created by dell on 2019/4/20.
+ */
+public class EchoClientHandler extends SimpleChannelInboundHandler<ByteBuf> {
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) {
+        ctx.writeAndFlush(Unpooled.copiedBuffer("Netty rocks!", CharsetUtil.UTF_8));
+    }
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext channelHandlerContext, ByteBuf in) throws Exception {
+        System.out.println("Client received: " + in.toString(CharsetUtil.UTF_8));
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
